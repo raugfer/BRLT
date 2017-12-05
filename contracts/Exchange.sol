20f1ed33a9ca9a8a3ba1f29ecda47e9d284ffba4
Rodrigo Augusto Barbato Ferreira <rodrigo.ferreira@aya.yale.edu>
2017-12-04 23:53:37 -0200

    I have added the AGPL copyright notice to every source file.

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,3 +1,24 @@
+/*
+ * BRLT, the Brazilian Real Token project
+ * Copyright (C) 2017 <rodrigo.ferreira@aya.yale.edu>
+ *
+ * This file is part of BRLT.
+ *
+ * BRLT is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * BRLT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with BRLT.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 // Interface for a simple ERC 20 Token exchange
 pragma solidity ^0.4.15;
 
