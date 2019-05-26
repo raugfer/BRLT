6b7712c00d1af8e65b32e094e656eae121729140
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2019-05-26 12:49:44 -0300

    Adds new definition of the Token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -21,21 +21,20 @@
 
 // Interface for the ERC 20 Token standard
 // https://github.com/ethereum/EIPs/issues/20
-pragma solidity 0.4.15;
+pragma solidity 0.4.17;
 
 contract Token
 {
 	// Optional
-	// function name() constant returns (string name);
-	// function symbol() constant returns (string symbol);
-	// function decimals() constant returns (uint8 decimals);
-	function totalSupply() public constant returns (uint256 _totalSupply);
-	function balanceOf(address _owner) public constant returns (uint256 _balance);
+	// function name() public view returns (string _name);
+	// function symbol() public view returns (string _symbol);
+	// function decimals() public view returns (uint8 _decimals);
+	function totalSupply() public view returns (uint256 _totalSupply);
+	function balanceOf(address _owner) public view returns (uint256 _balance);
 	function transfer(address _to, uint256 _value) public returns (bool _success);
 	function transferFrom(address _from, address _to, uint256 _value) public returns (bool _success);
 	function approve(address _spender, uint256 _value) public returns (bool _success);
-	function allowance(address _owner, address _spender) public constant returns (uint256 _remaining);
+	function allowance(address _owner, address _spender) public view returns (uint256 _remaining);
 	event Transfer(address indexed _from, address indexed _to, uint256 _value);
 	event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
-
